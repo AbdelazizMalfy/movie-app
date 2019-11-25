475ab21cedbaecb492c5f6482516e1f9e1cb3cf1
Abdelaziz <abdelazizMalfy@gmail.com>
2019-11-25 18:47:03 +0100

    mapping through movies list

--- components/card.js
+++ components/card.js
@@ -1,12 +1,11 @@
 const Card = (props) => {
     return(
         <div className="card h-100">
-            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+            <a href="#"><img className="card-img-top" src={props.img} alt="" /></a>
             <div className="card-body">
             <h4 className="card-title">
                 <a href="#"> {props.title} </a>
             </h4>
-            <h5> {props.price} </h5>
             <p className="card-text"> {props.text} </p>
             </div>
             <div className="card-footer">
