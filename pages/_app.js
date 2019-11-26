b4bbd187985b49ddcad27126855859e92b7aab38
Abdelaziz <abdelazizMalfy@gmail.com>
2019-11-26 12:56:36 +0100

    getting movies by id

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,23 @@ import Nav from '../components/nav';
 import Footer from '../components/footer';
 
 class MovieApp extends App {
+
+    static async getInitialProps(appContext){
+        //Excute getInitialProps of the page you are navigated to
+        const appProps = await App.getInitialProps(appContext)
+
+        return {
+            ...appProps
+        }
+    }
     
     render() {
-        const { Component } = this.props;
+        const { Component , pageProps } = this.props;
         
         return (
             <div>
                 <Head>
-                    <title>Home</title>
+                    <title>Movie DB</title>
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
                     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
@@ -21,7 +30,7 @@ class MovieApp extends App {
                 </Head>
                 <Nav/>
                 <div className='wrapper'>
-                    <Component/>
+                    <Component {...pageProps} />
                 </div>
                 <Footer/>
                 <style jsx>{`
