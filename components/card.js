6acebe6ea4e2bd0c3b1301f19dc1a514dd829f2b
Abdelaziz <abdelazizMalfy@gmail.com>
2019-11-27 09:47:41 +0100

    getting movie by ID

--- components/card.js
+++ components/card.js
@@ -3,12 +3,12 @@ import Link from 'next/link';
 const Card = (props) => {
     return(
         <div className="card h-100">
-            <Link  href={`movies/${props.id}`}>
+            <Link href='/movies/[id]' as={`movies/${props.id}`}>
                 <a href="#"><img className="card-img-top" src={props.img} alt="" /></a>
             </Link>
             <div className="card-body">
             <h4 className="card-title">
-                <Link  href={`movies/${props.id}`}>
+                <Link href='/movies/[id]'  as={`movies/${props.id}`}>
                     <a>{props.title}</a>
                 </Link>
             </h4>
